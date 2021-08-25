796ba7e2a67e835f86e566f85c66a9123c9316a9
nhi.ly <nhi.ly@be.com.vn>
2021-08-25 21:06:07 +0700

    fix: remove pinned city

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,74 +1,69 @@
-import { Layout, DivPx, ImageSlider, ImageTag } from 'components/common';
+import {
+  Layout,
+  DivPx,
+  ImageSlider,
+  ImageTag,
+  Loading,
+} from 'components/common';
 import { ENDPOINT_URL } from 'constants/api.const';
-import { IMAGES, GIFTS, ROOMS } from 'constants/images.const';
+import { IMAGES } from 'constants/images.const';
 import { SITE_PAGES } from 'constants/pages.const';
-import { DefaultCity, ICityInfo } from 'interfaces/city.interface';
 import { IImage } from 'interfaces/image.interface';
 import { useEffect, useState } from 'react';
-import { BASE, GET } from 'utils/fetcher.utils';
+import { GET } from 'utils/fetcher.utils';
 
 export default function Main(): JSX.Element {
-  const [citylist, setCityList] = useState<ICityInfo[]>([DefaultCity]);
-  const [imagelist, setImageList] = useState<IImage[]>([]);
+  const [recommended, setRecommended] = useState<IImage[]>([]);
+  const [loading, setLoading] = useState(false);
 
-  async function getPinnedCityData(){
-    const response = await GET(
-      ENDPOINT_URL.GET.getPinnedCity
-    );
-
-    if(response.status == 200){
-      if(response.data.valid === false) {
-        return;
-      }
-      setCityList(response.data.cities);
+  function getRecommendList() {
+    try {
+      setLoading(true);
+      setRecommended([]);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
   }
-  function getImageList(){
-    const list : IImage[] = [];
-    citylist.map((item, index)=>{
-      list.push({ 
-        _id: item.id, 
-        title: item.titles, 
-        path: BASE + item.thumnail, 
-        href: SITE_PAGES.LIST_OF_ROOMS.path + `/${item.titles.trim().replace(/ /g, '_')}`
-      });
-    });
-    setImageList(list);
-  }
 
   useEffect(() => {
-    getPinnedCityData();
+    getRecommendList();
   }, []);
-  useEffect(() => {
-    getImageList();
-  }, [citylist]);
+
   return (
-    <Layout allowSearch>
-      <ImageTag
-        data={{ path: '/images/welcome.jpg', _id: 'welcome' }}
-        width={100}
-      />
-      <DivPx size={32} />
-      <div className="px-32 text-lg">Welcome to 3211,</div>
-      {!localStorage.getItem('userID') && (
-        <div className="px-32 text-lg">
-          Please login or sign up to explore more!
-        </div>
+    <div>
+      {!loading ? (
+        <Layout allowSearch>
+          <ImageTag
+            data={{ path: '/images/welcome.jpg', _id: 'welcome' }}
+            width={100}
+          />
+          <DivPx size={32} />
+          <div className="px-32 text-lg">Welcome to 3211,</div>
+          {!localStorage.getItem('userID') && (
+            <div className="px-32 text-lg">
+              Please login or sign up to explore more!
+            </div>
+          )}
+          <DivPx size={48} />
+          <ImageSlider
+            title="SIGNIFICANT PLACES TO STAY"
+            limit={5}
+            images={IMAGES}
+            isLink={true}
+          />
+          <DivPx size={48} />
+          <ImageSlider
+            title="RECOMMENDED By 3211"
+            limit={4}
+            images={recommended}
+            isLink={true}
+          />
+        </Layout>
+      ) : (
+        <Loading />
       )}
-      <DivPx size={48} />
-      <ImageSlider
-        title="SIGNIFICANT PLACES TO STAY"
-        limit={5}
-        images={imagelist}
-        isLink={true}
-      />
-      <DivPx size={48} />
-      <ImageSlider
-        title="RECOMMENDED By 3211"
-        limit={4}
-        images={ROOMS}
-        isLink={true}
-      />
-    </Layout>
+    </div>
   );
 }
