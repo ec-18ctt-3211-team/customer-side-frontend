feb7930051a37117ebc26edb85d2d4ad4774e869
tcanh20102000 <tcanh18@apcs.vn>
2021-08-26 18:07:26 +0700

    Add Get Pinned City feat.

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -6,8 +6,8 @@ import {
   Loading,
 } from 'components/common';
 import { ENDPOINT_URL } from 'constants/api.const';
-import { IMAGES } from 'constants/images.const';
 import { SITE_PAGES } from 'constants/pages.const';
+import { DefaultCity, ICityInfo } from 'interfaces/city.interface';
 import { IImage } from 'interfaces/image.interface';
 import { IRoomDetail } from 'interfaces/room.interface';
 import { useEffect, useState } from 'react';
@@ -16,6 +16,49 @@ import { GET } from 'utils/fetcher.utils';
 export default function Main(): JSX.Element {
   const [recommended, setRecommended] = useState<IImage[]>([]);
   const [loading, setLoading] = useState(false);
+
+  const [citylist, setCityList] = useState<ICityInfo[]>([DefaultCity]);
+  const [imagelist, setImageList] = useState<IImage[]>([]);
+
+  async function getPinnedCityData(){
+    try{
+      setLoading(true);
+      const response = await GET(
+        ENDPOINT_URL.GET.getPinnedCity
+      );
+
+      if(response.status == 200){
+        if(response.data.valid === false) {
+          return;
+        }
+        setCityList(response.data.cities);
+      }
+    }
+    catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+  function getImageList(){
+    const list : IImage[] = [];
+    citylist.map((item, index)=>{
+      list.push({ 
+        _id: item.id, 
+        title: item.titles, 
+        path: item.thumnail, 
+        href: SITE_PAGES.LIST_OF_ROOMS.path + `/${item.titles.trim().replace(/ /g, '_')}`
+      });
+    });
+    setImageList(list);
+  }
+
+  useEffect(() => {
+    getPinnedCityData();
+  }, []);
+  useEffect(() => {
+    getImageList();
+  }, [citylist]);
 
   async function getRecommendList() {
     const userID = localStorage.getItem('userID');
@@ -66,7 +109,7 @@ export default function Main(): JSX.Element {
           <ImageSlider
             title="SIGNIFICANT PLACES TO STAY"
             limit={5}
-            images={IMAGES}
+            images={imagelist}
             isLink={true}
           />
           <DivPx size={48} />
